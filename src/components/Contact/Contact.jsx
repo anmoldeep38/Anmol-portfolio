d5a247d3eac491b94390c45d06f1aa2efe0ec36f
Anmol Deep <87927563+anmoldeep38@users.noreply.github.com>
2024-09-18 16:38:45 +0530

    Add files via upload

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:anmoldeep38@gmail.com">anmoldeep38@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/anmoldeep38/">linkedin.com/in/anmoldeep38/</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/anmoldeep38">github.com/anmoldeep38</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
